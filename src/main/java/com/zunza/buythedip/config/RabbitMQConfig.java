b728c0e347044bbf8bdb201fb7db9d0c2030f96e
zunza <fksfks015@naver.com>
2025-07-04 18:29:12 +0900

    feat: 바이낸스 데이터 관련 큐 추가

--- src/main/java/com/zunza/buythedip/config/RabbitMQConfig.java
+++ src/main/java/com/zunza/buythedip/config/RabbitMQConfig.java
@@ -3,12 +3,12 @@ package com.zunza.buythedip.config;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Declarable;
 import org.springframework.amqp.core.Declarables;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.QueueBuilder;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
@@ -21,7 +21,10 @@ public class RabbitMQConfig {
 
 	private final String[] queueNames = {
 		RabbitMQConstants.NEWS_TRANSLATION_QUEUE,
-		RabbitMQConstants.NEWS_STORAGE_QUEUE
+		RabbitMQConstants.NEWS_STORAGE_QUEUE,
+		RabbitMQConstants.TRADE_AGGREGATION_QUEUE,
+		RabbitMQConstants.TOP_VOLUME_TICKER_BROADCAST_QUEUE,
+		RabbitMQConstants.SYMBOL_TICKER_BROADCAST_QUEUE
 	};
 
 	@Bean
@@ -32,11 +35,16 @@ public class RabbitMQConfig {
 		declarables.add(exchange);
 
 		for (String queueName : queueNames) {
-			Queue queue = new Queue(queueName, true);
-			declarables.add(queue);
+			Queue mainQueue = QueueBuilder.durable(queueName).build();
+			declarables.add(mainQueue);
+			declarables.add(BindingBuilder.bind(mainQueue).to(exchange).with(RabbitMQConstants.getRoutingKey(queueName)));
 
-			Binding binding = BindingBuilder.bind(queue).to(exchange).with(RabbitMQConstants.getRoutingKey(queueName));
-			declarables.add(binding);
+			Queue retryQueue = QueueBuilder.durable(queueName + ".retry")
+				.withArgument("x-dead-letter-exchange", "")
+				.withArgument("x-dead-letter-routing-key", queueName)
+				.withArgument("x-message-ttl", 5000)
+				.build();
+			declarables.add(retryQueue);
 		}
 
 		return new Declarables(declarables);
