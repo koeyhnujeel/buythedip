f96f667b7560e29c6a67902d76835b372bb07633
zunza <fksfks015@naver.com>
2025-09-19 08:16:45 +0900

    ​

--- src/main/java/com/zunza/buythedip/infrastructure/event/event/UserRegisteredEvent.java
+++ src/main/java/com/zunza/buythedip/infrastructure/event/event/UserRegisteredEvent.java
@@ -2,17 +2,16 @@ package com.zunza.buythedip.infrastructure.event.event;
 
 import com.zunza.buythedip.user.entity.User;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class UserRegisteredEvent {
 	private User user;
-
-	@Builder
-	private UserRegisteredEvent(User user) {
-		this.user = user;
-	}
 
 	public static UserRegisteredEvent createFrom(User user) {
 		return UserRegisteredEvent.builder()
