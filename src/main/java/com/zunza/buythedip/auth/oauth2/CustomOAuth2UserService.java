b2ae5b2958e24bdad64970b0c23f2d2dcdd787dd
zunza <fksfks015@naver.com>
2025-09-18 23:34:14 +0900

    fix: 회원가입 시 기본 왓치리스트 미생성 버그 수정

--- src/main/java/com/zunza/buythedip/auth/oauth2/CustomOAuth2UserService.java
+++ src/main/java/com/zunza/buythedip/auth/oauth2/CustomOAuth2UserService.java
@@ -8,6 +8,7 @@ import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.zunza.buythedip.auth.oauth2.exception.DuplicateEmailWithDifferentProviderException;
 import com.zunza.buythedip.auth.oauth2.exception.SocialEmailAlreadyRegisteredException;
@@ -29,6 +30,7 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
 	private final ApplicationEventPublisher eventPublisher;
 
 	@Override
+	@Transactional
 	public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
 		OAuth2User oAuth2User = super.loadUser(userRequest);
 		String provider = userRequest.getClientRegistration().getRegistrationId();
