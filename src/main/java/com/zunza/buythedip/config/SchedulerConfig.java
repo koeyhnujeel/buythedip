921d8ee121aec82353ba0716d1bcf20cf6b41ff4
zunza <fksfks015@naver.com>
2025-09-19 09:14:52 +0900

    ​

--- src/main/java/com/zunza/buythedip/config/SchedulerConfig.java
+++ src/main/java/com/zunza/buythedip/config/SchedulerConfig.java
@@ -1,7 +1,9 @@
 package com.zunza.buythedip.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@Configuration
 @EnableScheduling
 public class SchedulerConfig {
 }
