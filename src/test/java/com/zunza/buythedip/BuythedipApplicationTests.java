d5cf26fce9db885835a095a86b36b4475bea1fd5
zunza <fksfks015@naver.com>
2025-09-29 16:54:11 +0900

    deploy test

--- src/test/java/com/zunza/buythedip/BuythedipApplicationTests.java
+++ src/test/java/com/zunza/buythedip/BuythedipApplicationTests.java
@@ -2,14 +2,8 @@ package com.zunza.buythedip;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
-import org.springframework.test.context.ActiveProfiles;
 
-import com.zunza.buythedip.config.TestContainersConfig;
-
-@SpringBootTest
-@ActiveProfiles("test")
-@Import(TestContainersConfig.class)
+// @SpringBootTest
 class BuythedipApplicationTests {
 
 	@Test
